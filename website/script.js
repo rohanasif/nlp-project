e5122c71c5cc62e7a37d020b0310253fba87d8c3
Rohan <rohanasif1990@gmail.com>
2022-10-20 19:37:55 +0500

    getting eventlistener error

--- website/script.js
+++ website/script.js
@@ -1,13 +1,21 @@
 const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
 const key = "a6cfb6bfd8bc63b829e4dbcfdb92ee2e";
-const submitBtn = document.getElementById("submitBtn");
 
+const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
     const url = document.getElementById("url").value;
+
     if (url !== "") {
         getData(baseURL, url, key)
+            .then(function (data) {
+                postData("/add", { data: data });
+            }).then(function () {
+                receiveData()
+            }).catch(function (error) {
+                console.log(error);
+                alert("Invalid input");
+            })
     }
 })
 
@@ -22,6 +30,37 @@ const getData = async (baseURL, url, key) => {
     }
 }
 
-const postData = async () => {
+const postData = async (url = "", data = {}) => {
+    const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            data: data
+        })
+    });
 
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const receiveData = async () => {
+    const request = await fetch('/all');
+    try {
+        // Transform into JSON
+        const allData = await request.json()
+        console.log(allData)
+        // Write updated data to DOM elements
+        document.getElementById('result').innerHTML = allData;
+    }
+    catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
 }
