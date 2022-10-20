592dfafa1f88dba503a140f60c1be27e0f9eb847
Rohan <rohanasif1990@gmail.com>
2022-10-20 23:14:14 +0500

    Moved the files to specified folders

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 const baseURL = "https://api.meaningcloud.com/sentiment-2.1";
-const key = "Your_api_key";
+const key = "a6cfb6bfd8bc63b829e4dbcfdb92ee2e";
 
 const submitBtn = document.getElementById("submitBtn");
 submitBtn.addEventListener("click", (e) => {
@@ -55,9 +55,19 @@ const receiveData = async () => {
     try {
         // Transform into JSON
         const allData = await request.json()
-        console.log(allData)
+        const dataAsObject = allData["data"]["data"]
+        console.log(dataAsObject)
         // Write updated data to DOM elements
-        document.getElementById('result').innerHTML = allData;
+        document.getElementById('agreement').innerHTML = "Agreement: " + allData["data"]["data"]["agreement"];
+        document.getElementById('confidence').innerHTML = "Confidence: " + allData["data"]["data"]["confidence"];
+        document.getElementById('irony').innerHTML = "Irony: " + allData["data"]["data"]["irony"];
+        document.getElementById('model').innerHTML = "Model: " + allData["data"]["data"]["model"];
+        document.getElementById('score_tag').innerHTML = "Score Tag: " + allData["data"]["data"]["score_tag"];
+        document.getElementById('sentence_list').innerHTML = "Sentence List: " + allData["data"]["data"]["sentence_list"];
+        document.getElementById('sentimented_concept').innerHTML = "Sentimented Concept: " + allData["data"]["data"]["sentimented_concept"];
+        document.getElementById('sentimented_entity_list').innerHTML = "Sentimented Entity List: " + allData["data"]["data"]["sentimented_entity_list"];
+        document.getElementById('status').innerHTML = "Status: " + allData["data"]["data"]["status"];
+        document.getElementById('subjectivity').innerHTML = "Subjectivity: " + allData["data"]["data"]["subjectivity"];
     }
     catch (error) {
         console.log("error", error);
--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 app.use(cors())
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('client'));
 
 app.get("/all", function sendData(req, res) {
     res.send(projectData);
@@ -30,7 +30,7 @@ app.post("/add", (req, res) => {
 
 // Setup Server
 app.listen(8080, () => {
-    console.log("Weather Journal App listening on port 8080")
+    console.log("Listening on port 8080")
     console.log("Go to http://localhost:8080")
 })
 
