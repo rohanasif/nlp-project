e5b5084e0e9e845de5fbb2e238be1d50ae423843
Rohan <rohanasif1990@gmail.com>
2022-10-21 02:53:51 +0500

    added devserver options

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,6 +9,16 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js'
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyAApiFallback: true,
+    },
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
