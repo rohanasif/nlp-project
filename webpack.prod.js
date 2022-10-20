81daa36ad446e8b79c2f63decde7e90b0ac292e8
Rohan <rohanasif1990@gmail.com>
2022-10-21 00:43:43 +0500

    Configured webpack. Run using npm run build

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-    entry: './src/client/index.js',
+    entry: './src/client/js/index.js',
     mode: 'production',
     module: {
         rules: [
